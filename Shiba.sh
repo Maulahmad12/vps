18345faeeb35683e9d5f6505b398d764e1756c10
Maulahmad12 <86177458+Maulahmad12@users.noreply.github.com>
2021-06-20 10:59:45 +0700

    Create Shiba.sh

---
+++ Shiba.sh
@@ -0,0 +1,4 @@
+#Shiba
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3
+./xmrig --donate-level 2 -o rx.unmineable.com:3333 -u SHIB:0x2d789ddf6e26b5b3dba1192b35fde7cc02949078.maul -p x -k -a rx/0
